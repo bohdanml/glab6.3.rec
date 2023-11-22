7a55208827003e69f104ffd1381ea39b56cf3fc7
Bogdan <Bogdan@VITOPC>
2023-11-22 16:31:24 +0200

    Add project files.

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\\lab6.3.rec\lab.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			const int size = 9;
+			int arr[size] = { 0 };  
+
+			fillArray(arr, size);
+
+			for (int i = 0; i < size; ++i)
+			{
+				Assert::AreEqual(i + 1, arr[i]);
+			}
+		}
+	};
+}
---
+++ lab6.3.rec/lab.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+// я обертання масиву
+void reverseArray(int arr[], int start, int end) {
+    if (start < end) {
+        // Обмін елементів
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        // Рекурсивний виклик 
+        reverseArray(arr, start + 1, end - 1);
+    }
+}
+
+//  виведення масиву на екран
+void printArray(int arr[], int size, int i = 0) {
+    if (i < size) {
+        cout << arr[i] << " ";
+        printArray(arr, size, i + 1);
+    }
+}
+// заповнення масиву послідовністю чисел
+void fillArray(int arr[], int size, int i = 0) {
+    if (i < size) {
+        arr[i] = i + 1;
+        fillArray(arr, size, i + 1);
+    }
+}
+
+
+int main() {
+    const int size = 9;
+    int arr[size];
+
+    // Заповнення  виведення оригінального масиву
+    fillArray(arr, size);
+    cout << "Original array: ";
+    printArray(arr, size);
+
+    // Обертання масиву
+    reverseArray(arr, 0, size - 1);
+    cout << "\nReversed array: ";
+    printArray(arr, size);
+
+    return 0;
+}
